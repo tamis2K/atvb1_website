31f57139c29d0ba6ebaada66ea97ea9f0e8773ad
Mateus Varnier <90485488+tamis2K@users.noreply.github.com>
2022-11-24 15:20:28 -0300

    24/11/2022

--- site-react/src/context/acoesContext.jsx
+++ site-react/src/context/acoesContext.jsx
@@ -15,9 +15,6 @@ const dados = [
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 902000.00,
         operacao: 'locacao',
-        tipo: 'apto',
-        cidade: 'beltrao',
-        bairro: 'centro'
     },
     {
         foto: './assets/img/thumb3.png',
@@ -25,19 +22,13 @@ const dados = [
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 903000.00,
         operacao: 'venda',
-        tipo: 'casa',
-        cidade: 'beltrao',
-        bairro: 'centro'
     },
     {
-        foto: './assets/img/thumb4.png',
+        foto: '',
         titulo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 904000.00,
         operacao: 'venda',
-        tipo: 'sobrado',
-        cidade: 'coronel',
-        bairro: 'centro'
     },
     {
         foto: './assets/img/thumb5.png',
@@ -45,9 +36,6 @@ const dados = [
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 905000.00,
         operacao: 'venda',
-        tipo: 'sobrado',
-        cidade: 'pato',
-        bairro: 'lasalle'
     },
     {
         foto: './assets/img/thumb6.png',
@@ -55,9 +43,6 @@ const dados = [
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 906000.00,
         operacao: 'venda',
-        tipo: 'apto',
-        cidade: 'pato',
-        bairro: 'centro'
     },
     {
         foto: './assets/img/thumb3.png',
@@ -65,9 +50,27 @@ const dados = [
         descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
         preco: 990000.00,
         operacao: 'locacao',
-        tipo: 'casa',
-        cidade: 'coronel',
-        bairro: 'centro'
+    },
+    {
+        foto: './assets/img/thumb3.png',
+        titulo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
+        preco: 903000.00,
+        operacao: 'venda',
+    },
+    {
+        foto: './assets/img/thumb3.png',
+        titulo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
+        preco: 903000.00,
+        operacao: 'venda',
+    },
+    {
+        foto: './assets/img/thumb3.png',
+        titulo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+        descricao: 'Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae. Mollitia itaque, facere corporis eaque voluptates eligendi recusandae consequuntur non ullam fugiat.Atque veniam explicabo qui eius? Aspernatur officiis sed nam quae.',
+        preco: 903000.00,
+        operacao: 'venda',
     }
 ]
 
